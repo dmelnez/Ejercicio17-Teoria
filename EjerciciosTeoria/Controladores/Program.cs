76b90c12dc9a5dfa64354ad1eafa57f7adc2e532
David Melendez <145343465+dmelnez@users.noreply.github.com>
2024-04-23 11:11:42 +0200

    Add files via upload

---
+++ EjerciciosTeoria/Controladores/Program.cs
@@ -0,0 +1,109 @@
+﻿using EjerciciosTeoria.Dtos;
+
+namespace EjerciciosTeoria.Controladores
+{
+
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            // EJERCICIO 16
+
+            List<EstudianteDtos> listaEstudiantes = new List<EstudianteDtos>();
+            Dictionary<string, EstudianteDtos> estudiantes = new Dictionary<string, EstudianteDtos>();
+            HashSet<EstudianteDtos> listaEstudiantesDuplicidad = new HashSet<EstudianteDtos>();
+
+
+            do
+            {
+                Console.WriteLine("[0] -> Buscar Estudiante");
+                Console.WriteLine("[1] -> Añadir Estudiante");
+                int seleccion = Convert.ToInt32(Console.ReadLine());
+
+
+                switch (seleccion)
+                {
+                    case 0:
+
+                        Console.WriteLine("Nombre Estudiante: ");
+                        string nombreEstudiante = Console.ReadLine();
+
+                        foreach(var kvp in estudiantes)
+                        {
+                            if (kvp.Key == nombreEstudiante)
+                            {
+                                Console.WriteLine("Estudiante encontrado: " + nombreEstudiante);
+
+                            }
+
+                            else
+                            {
+
+                                Console.WriteLine("Usuario no encontrado");
+
+                            }
+
+                        }
+
+                        break;
+                    case 1:
+
+
+                        EstudianteDtos estuadiante = new EstudianteDtos();
+
+                        Console.WriteLine("Nombre: ");
+
+                        estuadiante.Nombre = Console.ReadLine();
+
+                        Console.WriteLine("Apellido: ");
+                        estuadiante.Apellido = Console.ReadLine();
+
+                        Console.WriteLine("Edad: ");
+                        estuadiante.Edad = Convert.ToInt32(Console.ReadLine());
+
+                        Console.WriteLine("Grado: ");
+                        estuadiante.Grado = Console.ReadLine();
+
+
+
+                        listaEstudiantes.Add(estuadiante);
+                        listaEstudiantesDuplicidad.Add(estuadiante);
+
+                        estudiantes.Add(estuadiante.Nombre, estuadiante);
+
+
+
+                        break;
+
+                    default:
+                        Console.WriteLine("SELECION NO VALIDA");
+                        break;
+
+                }
+
+            }
+
+            while (true);
+
+
+
+            foreach(var estudiante in listaEstudiantesDuplicidad)
+            {
+
+                Console.WriteLine("Nombre: " + estudiante.Nombre + " Apellidos: " + estudiante.Apellido + " Edad: " + estudiante.Edad + " Grado: " + estudiante.Grado);
+
+            }
+
+
+
+
+
+        }
+
+
+
+    }
+
+}
+
---
+++ EjerciciosTeoria/Dtos/EstudianteDtos.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciciosTeoria.Dtos
+{
+    internal class EstudianteDtos
+    {
+
+        long id;
+
+        string nombre = "aaaaa";
+
+        string apellido = "aaaaa";
+
+        int edad = 11111;
+
+        string grado = "aaaaa";
+
+        public long Id { get => id; set => id = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellido { get => apellido; set => apellido = value; }
+        public int Edad { get => edad; set => edad = value; }
+        public string Grado { get => grado; set => grado = value; }
+
+        public EstudianteDtos(long id, string nombre, string apellido, int edad, string grado)
+        {
+            this.id = id;
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.edad = edad;
+            this.grado = grado;
+        }
+
+        public EstudianteDtos() { }
+
+
+    }
+}
